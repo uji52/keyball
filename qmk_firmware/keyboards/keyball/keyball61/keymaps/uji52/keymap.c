1eac944fa0611709db2a2bb2427f0fe637a7781e
uji52 <uji.52724@gmail.com>
2022-11-30 23:34:11 +0900

    メールアドレスの入力とsmall fix

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/uji52/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/uji52/keymap.c
@@ -20,19 +20,25 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "quantum.h"
 
-// clang-format off
+
+enum custom_keycodes {
+  MAIL1,
+  MAIL2
+};
+
+// clang-format offI
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_universal(
     KC_ESC   , KC_1     , KC_2     , KC_3     , KC_4     , KC_5     ,                                  KC_6     , KC_7     , KC_8     , KC_9     , KC_0     , KC_MINS  ,
     KC_TAB   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                  KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_EQL   ,
     KC_LCTL  , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                  KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , KC_QUOT  ,
-    KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     , KC_RBRC  ,              KC_NUHS, KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_BSLS  ,
+    KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     , KC_RBRC  ,              KC_LBRC, KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_BSLS  ,
     KC_LALT  , TO(1)    , TO(3)    , KC_CAPS  , MO(2)    , MO(1)    , KC_SPC   ,              KC_ENT , KC_BSPC  , _______  , _______  , _______  , KC_DEL   , KC_GRV 
   ),
 
   [1] = LAYOUT_universal(
     KC_F1    , KC_F2    , KC_F3    , KC_F4    , KC_F5    , KC_F6    ,                                  KC_F7    , KC_F8    , KC_F9    , KC_F10   , KC_F11   , KC_F12   ,
-    _______  , _______  , _______  , _______  , _______  , _______  ,                                  KC_WH_L  , KC_WH_D  , KC_WH_U  , KC_WH_R  , _______  , _______  ,
+    _______  , MAIL1    , MAIL2    , _______  , _______  , _______  ,                                  KC_WH_L  , KC_WH_D  , KC_WH_U  , KC_WH_R  , _______  , _______  ,
     _______  , _______  , _______  , _______  , _______  , KC_LGUI  ,                                  KC_LEFT  , KC_DOWN  , KC_UP    , KC_RGHT  , _______  , _______  ,
     _______  , _______  , _______  , _______  , _______  , _______  , _______  ,            _______  , _______  , KC_BTN1  , KC_BTN3  , KC_BTN2  , _______  , _______  ,
     _______  , TO(2)    , TO(0)    , _______  , _______  , _______  , _______  ,            _______  , KC_WBAK  , _______  , _______  , _______  , _______  , _______
@@ -71,3 +77,22 @@ void oledkit_render_info_user(void) {
     keyball_oled_render_ballinfo();
 }
 #endif
+
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case MAIL1:
+      if (record->event.pressed) {
+        SEND_STRING("uji.52724@gmail.com");
+      }
+      break;
+    case MAIL2:
+      if (record->event.pressed) {
+        SEND_STRING("test@uji52.com");
+      }
+      break;
+  }
+  return true;
+}
+
+
